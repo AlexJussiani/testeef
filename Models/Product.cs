a65be7b1c8c07e66cb5e5ad408caf2536af683fd
alex jussiani junior <alexjunior_jussiani@hotmail.com>
2021-04-10 12:52:09 -0300

    first commit

---
+++ Models/Product.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using testeef.Models;
+
+namespace testeef.Models
+{
+  public class Product
+  {
+    [Key]
+      public int Id { get; set; }
+
+    [Required(ErrorMessage = "Este Campo é obrigatório")]
+    [MaxLength(60, ErrorMessage = "Este campo deve conter entre 3 a 60 caracteres")]
+    [MinLength(3, ErrorMessage = "Este campó deve conter entre 3 e 0 caracteres")]
+
+    public string Title {get; set ;}
+
+    [MaxLength(1024, ErrorMessage = "Este campo deve conter entre 3 a 60 caracteres")]
+    public string Description {get; set ;}
+
+    [Required(ErrorMessage = "Este Campo é obrigatório")]
+    [Range(1, int.MaxValue, ErrorMessage = "O preço deve ser maior que zero")]
+    public string Price {get; set ;}
+
+     [Required(ErrorMessage = "Este Campo é obrigatório")]
+    [Range(1, int.MaxValue, ErrorMessage = "Categoria inválida")]
+    public string CategoryId {get; set ;}
+
+    public Category Category {get; set ;}
+  }
+}
