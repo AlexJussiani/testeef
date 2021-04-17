a56243442cb8594246d7dea39847b6460618c8f1
alex <alex@FORLOGIC657>
2021-04-17 13:03:20 -0300

    Envio

--- Models/Product.cs
+++ Models/Product.cs
@@ -17,13 +17,13 @@ namespace testeef.Models
     [MaxLength(1024, ErrorMessage = "Este campo deve conter entre 3 a 60 caracteres")]
     public string Description {get; set ;}
 
-    [Required(ErrorMessage = "Este Campo é obrigatório")]
-    [Range(1, int.MaxValue, ErrorMessage = "O preço deve ser maior que zero")]
+    //[Required(ErrorMessage = "Este Campo é obrigatório")]
+    //[Range(1, int.MaxValue, ErrorMessage = "O preço deve ser maior que zero")]
     public string Price {get; set ;}
 
      [Required(ErrorMessage = "Este Campo é obrigatório")]
-    [Range(1, int.MaxValue, ErrorMessage = "Categoria inválida")]
-    public string CategoryId {get; set ;}
+     [Range(1, int.MaxValue, ErrorMessage = "Categoria inválida")]
+    public int CategoryId {get; set ;}
 
     public Category Category {get; set ;}
   }
